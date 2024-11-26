29260509ea378873e95cd9cbf199c9c7b1b36e95
bendera <szoftverhiba@gmail.com>
2024-11-27 00:00:45 +0100

    Add v-model examples

--- vue-example/src/main.ts
+++ vue-example/src/main.ts
@@ -1,4 +1,18 @@
 import './assets/main.css'
+import '@vscode-elements/elements/dist/vscode-button'
+import '@vscode-elements/elements/dist/vscode-checkbox'
+import '@vscode-elements/elements/dist/vscode-form-group'
+import '@vscode-elements/elements/dist/vscode-label'
+import '@vscode-elements/elements/dist/vscode-single-select'
+import '@vscode-elements/elements/dist/vscode-multi-select'
+import '@vscode-elements/elements/dist/vscode-option'
+import '@vscode-elements/elements/dist/vscode-radio'
+import '@vscode-elements/elements/dist/vscode-radio-group'
+import '@vscode-elements/elements/dist/vscode-textfield'
+
+if (import.meta.env.DEV) {
+  await import('@vscode-elements/webview-playground')
+}
 
 import { createApp } from 'vue'
 import App from './App.vue'
--- vue-example/vite.config.ts
+++ vue-example/vite.config.ts
@@ -7,12 +7,29 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          // treat all tags with a dash as custom elements
+          isCustomElement: (tag) => tag.includes('-'),
+        },
+      },
+    }),
     vueDevTools(),
+    {
+      name: 'html-transform',
+      apply: 'build',
+      transformIndexHtml(html) {
+        return html.replace(
+          '<script type="module" src="node_modules/@vscode-elements/webview-playground/dist/index.js"></script>',
+          '',
+        )
+      },
+    },
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
